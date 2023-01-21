aba0c1ba53b2d52b39f1812359a80c4fcd123e8f
VincenzoParrinelli <vincenzoparrinelli01@gmail.com>
2023-01-21 18:31:44 +0100

    feat(dashboard): styling

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -1,4 +1,5 @@
-import React, { useState, ReactElement, useContext, ChangeEvent } from 'react'
+import React, { useState, useRef, ReactElement, useContext, ChangeEvent, DragEvent } from 'react'
+import { v4 as uuidv4 } from "uuid"
 import { PeerConnectionContext } from '../Contexts/PeerConnection'
 import "./Dashboard.scss"
 
@@ -6,8 +7,13 @@ export default function Dashboard(): ReactElement {
 
     const [file, setFile] = useState<File | null>(null)
 
+    const draggableAreaRef = useRef(null) as React.MutableRefObject<HTMLDivElement | null>
+
     // Get peer connection from context
     const pc = useContext(PeerConnectionContext) as RTCPeerConnection
+
+
+    const getClientCode = () => uuidv4()
 
     const handleFile = (e: ChangeEvent): void => {
 
@@ -17,32 +23,66 @@ export default function Dashboard(): ReactElement {
 
     }
 
+    const handleDragOver = (e: DragEvent): void => {
+        e.preventDefault()
+
+        draggableAreaRef.current?.classList.add("dashboard__file-dragger--drag-over")
+
+    }
+
+    const handleDragLeave = (e: DragEvent): void => {
+        e.preventDefault()
+
+        draggableAreaRef.current?.classList.remove("dashboard__file-dragger--drag-over")
+    }
+
+    const handleOnDrop = (e: DragEvent): void => {
+        e.preventDefault()
+
+        if (!e.dataTransfer.items) return
+
+        [...e.dataTransfer.items].forEach(item => {
+
+            setFile(item.getAsFile())
+
+        })
+
+        draggableAreaRef.current?.classList.remove("dashboard__file-dragger--drag-over")
+    }
+
     return (
         <div className='dashboard'>
 
-            <h1>UPLOAD FILE</h1>
+            <h1 className='dashboard__upload-file-heading'>UPLOAD FILE</h1>
+
+            <p className='dashboard__your-code-paragraph'>Your Code:</p>
+
+            <span className='dashboard__client-code'>{getClientCode()}</span>
 
             <div
-
+                ref={draggableAreaRef}
                 className='dashboard__file-dragger'
-              
+                onDragOver={e => handleDragOver(e)}
+                onDragLeave={e => handleDragLeave(e)}
+                onDrop={e => handleOnDrop(e)}
             >
 
-                DRAG FILE HERE
+                DRAG FILE HERE 
+
+                <p className='dashboard__or-paragraph'>OR</p>
+
+                <label htmlFor="file" className='dashboard__browse-paragraph'>BROWSE</label>
+
+                <input
+                    id='file'
+                    type="file"
+                    className='dashboard__file-input'
+                    onChange={e => handleFile(e)}
+                />
 
             </div>
 
-            <p className='dashboard__or-paragraph'> or </p>
-
-            <label htmlFor="file" className='dashboard__browse-btn'>Browse</label>
-
-            <input
-                id='file'
-                type="file"
-                className='dashboard__file-input'
-                onChange={e => handleFile(e)}
-            />
-
+            <button className='dashboard__send-btn'>SEND</button>
 
         </div>
     )
